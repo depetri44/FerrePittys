561b76b2b183abc4a9ca0feb21197d0e92459228
depetri44 <120295465+depetri44@users.noreply.github.com>
2023-06-17 21:38:10 -0300

    Initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { SnackbarProvider } from 'notistack';
+import { CssBaseline } from '@mui/material';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( 
+  <React.StrictMode>
+    <Provider store={store}>
+      <SnackbarProvider
+        maxSnack={2}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <Router>
+          
+        <CssBaseline />
+          <App />
+        </Router>
+      </SnackbarProvider>
+    </Provider>
+  </React.StrictMode> 
+);
